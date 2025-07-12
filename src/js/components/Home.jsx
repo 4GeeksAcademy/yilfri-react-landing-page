800c4fc54cd94f3c59771c46cf02e1800282e945
Yilfri Salave <yilfri97@hotmail.com>
2025-07-12 00:52:46 +0000

    Home added

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -1,27 +1,27 @@
 import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import Navbar from "./Navbar";
+import Jumbotron from "./Jumbotron";
+import Card from "./Card";
 
 //create your first component
 const Home = () => {
 	return (
-		<div className="text-center">
-            
+		<>
+			<Navbar />
+			<div className="container ">
+				<Jumbotron />
+				<div className="row row-cols-1 row-cols-md-4 py-4 g-4">
+					<Card />
+					<Card />
+					<Card />
+					<Card />
+				</div>
+			</div>
+			<div className="bg-dark text-white p-3 text-center">
+				<p>Copyright@ Yilfri Salave 2025</p>
+			</div>
 
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		</>
 	);
 };
 
