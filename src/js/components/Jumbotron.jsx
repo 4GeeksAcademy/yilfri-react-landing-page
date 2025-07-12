2a2ac4e3d353059e7d7d2274488c7f06b61355b3
Yilfri Salave <yilfri97@hotmail.com>
2025-07-12 00:53:48 +0000

    Jumbotron added

---
+++ src/js/components/Jumbotron.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+const Jumbotron = () => {
+    return (
+        <div className="jumbotron bg-secondary-subtle mt-4 px-3 py-5 rounded">
+            <h1 className="fw-semibold display-3">A Warm Welcome!</h1>
+            <p className="lead fs-6">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat provident inventore facilis nam, nulla doloribus officia illum, distinctio tenetur, doloremque temporibus? Aut, velit magni. Debitis blanditiis enim corrupti cumque sit.
+            </p>
+            <a className="btn btn-primary btn-lg mt-1 mb-4 fs-6 fw-semibold" href="#" role="button">Call to action!</a>
+        </div>
+    )
+}
+
+export default Jumbotron;
